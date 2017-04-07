05f794a553485bd92b99071c60875c7e59dfd4d7
akihiro mochizuki <a.mochizuki13@gmail.com>
2017-04-07 22:00:46 +0000

    CRUD tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,63 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+
+	def new
+    @task = Task.new
+	end
+
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = "正常に登録されました。"
+      redirect_to @task
+    else 
+      flash.now[:danger] = "登録できませんでした。"
+      render :new
+    end
+  end
+
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "正常に登録されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "登録されませんでした。"
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "正常に削除されました。"
+    redirect_to tasks_url
+  end
+
+
+  private
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
