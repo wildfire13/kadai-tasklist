1c5484887276a4bf60d2db7fe443f1b9b3e711f7
akihiro mochizuki <a.mochizuki13@gmail.com>
2017-04-13 15:15:33 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |n|
+  Task.create(status: "task_" + n.to_s, content: "task_content" + n.to_s)
+end
